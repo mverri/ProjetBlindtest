128fc0b69c70df97d02d9f57c91ee63a3b065662
mverri <verrier.mathilde92@gmail.com>
2014-09-26 20:22:30 +0200

    projet audio
    ajout d'une lecture d'un fichier mp3 aléatoire a partir de la racine
    d'une bibliotheque

---
+++ TPGPmp3/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        MusicPlayer player = new MusicPlayer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+           // openFileDialog1.ShowDialog();
+        }      
+      
+       
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "C:\\Users\\Chantal\\Desktop\\Michael Jackson\\01 Bad.mp3";
+            player.open("C:\\Users\\Chantal\\Desktop\\Michael Jackson");
+            player.play();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            player.stop();
+        }
+    }
+    
+}
---
+++ TPGPmp3/WpfApplication1/MusicPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+
+
+namespace WpfApplication1
+{
+    class MusicPlayer
+    {
+        // importation a faire car c# ne permet pas la lecture de .mp3, en fait on fait appel à l'OS.
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string lpstrCommand, StringBuilder lpstrReturnString, int uReturnLength, int hwndCallback);
+        // je ne sais pas a quoi servent les deux derniers arguments
+
+        // recupere la liste des chanson d'un repertoire en parcourant toute son arborescence
+        public IEnumerable<FileInfo> listeChanson (string path)
+        {
+            var dir = new DirectoryInfo(path);
+            var extensions = new string[] { ".mp3", ".wma" }; // choix dse extension
+            var mp3Files = dir.GetFiles("*.*").Where(f => extensions.Contains(f.Extension.ToLower())); // le *.* permet de dire que tu cherche un fic
+            var sousDossier = dir.GetDirectories();            
+            foreach(DirectoryInfo path2 in sousDossier){
+                mp3Files = mp3Files.Union(listeChanson(path2.FullName));
+            }
+            return mp3Files;
+        }
+
+        // Cette fonction ouvre une chanson aléatoire a aprtir du chemin d'un repertoire
+        public void open(string path)
+        {           
+        string file = null;
+        if (!string.IsNullOrEmpty(path))
+        {
+            try
+            {
+                var songsList = listeChanson(path);
+                Random R = new Random();
+                file = songsList.ElementAt(R.Next(0, songsList.Count())).FullName;
+            }
+            catch { }
+        }            
+            string command = "open \"" + file + "\" type MPEGVideo alias MyMp3"; // c'est une commande pour ouvrir le fichier
+            mciSendString(command, null, 0, 0);
+        }
+
+        // lancer la musique
+        public void play()
+        {
+            string command = "play MyMp3";
+            mciSendString(command, null, 0, 0);
+        }
+
+        // arreter la musique
+        public void stop()
+        {
+            string command = "stop MyMp3";
+            mciSendString(command, null, 0, 0); // il faut arreter la musique avant de fermer le fichier
+            command = "close MyMp3";
+            mciSendString(command, null, 0, 0); // fermeture du fichier
+        }
+    }
+   
+}
